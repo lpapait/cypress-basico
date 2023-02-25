1f8ba1e1f552423b4abb1f1d5516a18bcb440283
Lucas Papait <lucas.papait@gmail.com>
2023-02-25 19:13:51 -0300

    Exercicio 0

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,14 +1,19 @@
-
 //<reference typress='Cypress'/>
-
 describe('Central de Atendimento ao Cliente TAT', () => {
-    beforeEach( () => {
-        it('Deve visitar o site', () => {
-            cy.visit('./src/index.html')
-        })
+    beforeEach( ()=> {
+        cy.visit('./src/index.html')
     })
 
     it('Verificar o título da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('Preenche os campos obrigatórios e envia o formulário', ()=> {
+        cy.get('#firstName').click().type('Lucas')
+        cy.get('#lastName').click().type('Papait Desiderio')
+        cy.get('#email').click().type('lucas@papait.com')
+        cy.get('#open-text-area').click().type('Pignut hamburg parsley tigernut.')
+        cy.get('.button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
     })
 })
