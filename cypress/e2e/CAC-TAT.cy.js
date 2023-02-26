c3d7d92db25d1a9000b49dbf28f57a50e34d44c9
Lucas Papait <lucas.papait@gmail.com>
2023-02-26 00:11:56 -0300

    aula 1 / cypress 12.7.0

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,12 +8,18 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('Preenche os campos obrigatórios e envia o formulário', ()=> {
+    it('Preenche os campos obrigatórios e envia o formulário', ()=> {
+        const longText = 'Pignut hamburg parsley tigernut.,Pignut hamburg parsley tigernut.,Pignut hamburg parsley tigernut.'
         cy.get('#firstName').click().type('Lucas')
         cy.get('#lastName').click().type('Papait Desiderio')
         cy.get('#email').click().type('lucas@papait.com')
-        cy.get('#open-text-area').click().type('Pignut hamburg parsley tigernut.')
+        cy.get('#open-text-area').click().type(longText, {delay:0})
         cy.get('.button[type="submit"]').click()
         cy.get('.success').should('be.visible')
+    })
+
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=> {
+        cy.get('.button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
     })
 })
