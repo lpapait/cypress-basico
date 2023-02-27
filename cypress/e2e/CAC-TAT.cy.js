a654ff987f77451ac38bdd8cd1396ac3f7e82cdc
Lucas Papait <lucas.papait@gmail.com>
2023-02-26 21:12:17 -0300

    correção json

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -169,7 +169,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     })
 
     it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
-        cy.fixture('example.jason').as('sampleFile')
+        cy.fixture('example.json').as('sampleFile')
         cy.get('input[type="file"]')
             .selectFile('@sampleFile')
             .should(($input) => {
